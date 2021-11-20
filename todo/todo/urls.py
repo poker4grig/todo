d8ccf04d797022313860143a0638ca3452fef212
Sergey <poker4grig@yandex.ru>
2021-11-20 22:38:58 +0300

    add App, UserItem, UserList in frontend app. Combined and launched frontend b backend.

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter, SimpleRouter
+from rest_framework.routers import DefaultRouter
 
 from users.views import UserModelViewSet
 
