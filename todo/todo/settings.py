0ff18d455745f302d3eccab4bdb20b0739c9fafe
Sergey <poker4grig@yandex.ru>
2021-11-21 19:15:16 +0300

    fixed bugs with bootstrap

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'django-insecure-g+91+%#@#&4lw$w&zw54%el*$@rd*x!y)xgc&rz+(pj@sou5!!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,13 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'users',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
