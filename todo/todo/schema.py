9b255a82e168ee37b370b9a196c1ad961982fc27
Sergey <poker4grig@yandex.ru>
2021-12-20 19:32:01 +0300

    done Mutation

--- todo/todo/schema.py
+++ todo/todo/schema.py
@@ -6,28 +6,24 @@ from project.models import Project, Todo
 
 
 class UserType(DjangoObjectType):
-
     class Meta:
         model = User
         fields = '__all__'
 
 
 class ProjectType(DjangoObjectType):
-
     class Meta:
         model = Project
         fields = '__all__'
 
 
 class TodoType(DjangoObjectType):
-
     class Meta:
         model = Todo
         fields = '__all__'
 
 
 class Query(ObjectType):
-
     all_users = graphene.List(UserType)
     all_projects = graphene.List(ProjectType)
     all_todoes = graphene.List(TodoType)
@@ -79,17 +75,25 @@ class Query(ObjectType):
 
 class UserUpdateMutation(graphene.Mutation):
     class Arguments:
-        last_name = graphene.String(required=True)
-        first_name = graphene.String(required=True)
+        username = graphene.String()
+        last_name = graphene.String()
+        first_name = graphene.String()
+        email = graphene.String()
+        is_stuff = graphene.Boolean()
+        is_active = graphene.Boolean()
         id = graphene.ID()
 
     user = graphene.Field(UserType)
 
     @classmethod
-    def mutate(cls, root, info, last_name, first_name, id):
+    def mutate(cls, root, info, username, first_name, last_name, email, is_stuff, is_active, id):
         user = User.objects.get(id=id)
-        user.last_name = last_name
+        user.username = username
         user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.is_stuff = is_stuff
+        user.is_active = is_active
         user.save()
         return UserUpdateMutation(user=user)
 
@@ -97,15 +101,18 @@ class UserUpdateMutation(graphene.Mutation):
 class UserCreateMutation(graphene.Mutation):
     class Arguments:
         username = graphene.String(required=True)
-        first_name = graphene.String(required=True)
-        last_name = graphene.String(required=True)
+        first_name = graphene.String(required=False)
+        last_name = graphene.String(required=False)
         email = graphene.String(required=True)
+        is_stuff = graphene.Boolean(required=False)
+        is_active = graphene.Boolean(required=False)
 
     user = graphene.Field(UserType)
 
     @classmethod
-    def mutate(cls, root, info, username, first_name, last_name, email):
-        user = User.objects.create(username=username, first_name=first_name, last_name=last_name, email=email)
+    def mutate(cls, root, info, username, first_name, last_name, email, is_stuff, is_active):
+        user = User.objects.create(username=username, first_name=first_name, last_name=last_name, email=email,
+                                   is_stuff=is_stuff, is_active=is_active)
         return UserCreateMutation(user=user)
 
 
@@ -122,10 +129,114 @@ class UserDeleteMutation(graphene.Mutation):
         return UserDeleteMutation(user=user)
 
 
+class ProjectUpdateMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        user = graphene.List(graphene.Int)
+        repo = graphene.String()
+        id = graphene.ID()
+
+    project = graphene.Field(ProjectType)
+
+    @classmethod
+    def mutate(cls, root, info, name, user, repo, id):
+        project = Project.objects.get(id=id)
+        project.name = name
+        project.user.set([*user])
+        project.repo = repo
+        return ProjectUpdateMutation(project=project)
+
+
+class ProjectCreateMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        user = graphene.List(graphene.Int)
+        repo = graphene.String()
+
+    project = graphene.Field(ProjectType)
+
+    @classmethod
+    def mutate(cls, root, info, name, user, repo):
+        project = Project.objects.create(name=name, repo=repo)
+        project.user.set([*user])
+        return ProjectCreateMutation(project=project)
+
+
+class ProjectDeleteMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    project = graphene.List(ProjectType)
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        Project.objects.get(id=id).delete()
+        project = Project.objects.all()
+        return ProjectDeleteMutation(project=project)
+
+
+class TodoUpdateMutation(graphene.Mutation):
+    class Arguments:
+        user = graphene.Int(required=True)
+        project = graphene.Int(required=True)
+        text = graphene.String()
+        is_active = graphene.Boolean()
+        id = graphene.ID()
+
+    todo = graphene.Field(TodoType)
+
+    @classmethod
+    def mutate(cls, root, info, user, project, text, is_active, id):
+        user = User.objects.get(id=user)
+        project = Project.objects.get(id=project)
+        todo = Todo.objects.get(id=id)
+        todo.user = user
+        todo.project = project
+        todo.text = text
+        todo.is_active = is_active
+        return TodoUpdateMutation(todo=todo)
+
+
+class TodoCreateMutation(graphene.Mutation):
+    class Arguments:
+        user = graphene.Int(required=True)
+        project = graphene.Int(required=True)
+        text = graphene.String()
+        is_active = graphene.Boolean()
+
+    todo = graphene.Field(TodoType)
+
+    @classmethod
+    def mutate(cls, root, info, user, project, text, is_active):
+        user = User.objects.get(id=user)
+        project = Project.objects.get(id=project)
+        todo = Todo.objects.create(user=user, project=project, text=text, is_active=is_active)
+        return TodoCreateMutation(todo=todo)
+
+
+class TodoDeleteMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    todo = graphene.List(TodoType)
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        Todo.objects.get(id=id).delete()
+        todo = Todo.objects.all()
+        return TodoDeleteMutation(todo=todo)
+
+
 class Mutation(ObjectType):
     update_user = UserUpdateMutation.Field()
     create_user = UserCreateMutation.Field()
     delete_user = UserDeleteMutation.Field()
+    update_project = ProjectUpdateMutation.Field()
+    create_project = ProjectCreateMutation.Field()
+    delete_project = ProjectDeleteMutation.Field()
+    update_todo = TodoUpdateMutation.Field()
+    create_todo = TodoCreateMutation.Field()
+    delete_todo = TodoDeleteMutation.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
